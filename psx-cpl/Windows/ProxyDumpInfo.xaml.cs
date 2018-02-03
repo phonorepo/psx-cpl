061570d072348de0e719f0f08b5f22a81f8582e8
phonorepo <phono@keemail.me>
2018-02-03 07:48:16 +0000

    add Proxy Warning Window

---
+++ psx-cpl/Windows/ProxyDumpInfo.xaml.cs
@@ -0,0 +1,68 @@
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace psx_cpl.Windows
+{
+    public partial class ProxyDumpInfo : Window, INotifyPropertyChanged
+    {
+        public ProxyDumpInfo()
+        {
+            InitializeComponent();
+            DataContext = MainWindow.Instance;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private int _value = 0;
+        private string _text = "?";
+
+        public int Value
+        {
+            get { return _value; }
+        }
+        
+        private void ListViewScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
+        public enum DialogValues
+        {
+            No = 0,
+            Yes = 1,
+            NoToAll = 2,
+            YesToAll = 3,
+            Cancel = 4,
+            OK = 5
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = ((Button)sender).Tag;
+            _value = Int32.Parse(tag.ToString());
+            this.Close();
+        }
+
+         private void Window_Closed(object sender, EventArgs e)
+        {
+            //MainWindow.Instance.WindowLog = null;
+            MainWindow.Instance.WindowLogIsOpen = false;
+        }
+
+    }
+}
